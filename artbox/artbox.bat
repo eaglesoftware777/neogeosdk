dd45eca3f8ac51731da7d6961c6d60f14bb1de6a
eaglesoftware777 <93874722+eaglesoftware777@users.noreply.github.com>
2021-12-07 18:52:31 +0100

    Update artbox.bat

--- artbox/artbox.bat
+++ artbox/artbox.bat
@@ -1,10 +1,10 @@
-python createromdb.py
-python romdbimgimport.py 10
-rem python romdbfiximport.py
-rem python fixtiles.py
-python romtiles.py
-python genmapfile.py 10
-python genmapdb.py
-python genscreens.py 10 16 16 16
+py -2.7 createromdb.py
+py -2.7 romdbimgimport.py 10
+rem py -2.7 romdbfiximport.py
+rem py -2.7 fixtiles.py
+py -2.7 romtiles.py
+py -2.7 genmapfile.py 10
+py -2.7 genmapdb.py
+py -2.7 genscreens.py 10 16 16 16
 rem call romfx.bat
 call romts.bat
