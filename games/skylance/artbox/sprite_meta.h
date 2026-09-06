bbbaa2e28259b0da436aac780ad1645cfc330ace
eaglesoftware777 <eagle@eaglesoftware.biz>
2026-09-06 23:56:22 +0100

    build: regenerate art, sound tables and the ROM set for all six games
    Every game rebuilt from source with the pipeline changes in place: linear
    light resampling, set-distance palette banks, HD conditioning on by
    default, and the new per-asset import ceilings.  MVS for the shipped P
    ROMs, with an AES link verified for each and the MVS P ROM put back
    afterwards, since both platforms write the same file.
    
    19 builds, no errors, no warnings.  All six ROM sets complete; Sky Lance's
    V ROM stays at its full 4135936 bytes.

--- games/skylance/artbox/sprite_meta.h
+++ games/skylance/artbox/sprite_meta.h
@@ -32,55 +32,55 @@ typedef struct {
 #define NG_ASSET_STRIPS_1 16  /* background_coast_city.png */
 #define NG_ASSET_STRIPS_2 16  /* background_sky_mountains.png */
 #define NG_ASSET_STRIPS_3 10  /* sprite_p1_pilot.png */
-#define NG_ASSET_STRIPS_4 8  /* sprite_p1_plane.png */
+#define NG_ASSET_STRIPS_4 2  /* sprite_p1_plane.png */
 #define NG_ASSET_STRIPS_5 10  /* sprite_p2_pilot.png */
-#define NG_ASSET_STRIPS_6 8  /* sprite_p2_plane.png */
+#define NG_ASSET_STRIPS_6 2  /* sprite_p2_plane.png */
 #define NG_ASSET_STRIPS_7 10  /* sprite_p3_pilot.png */
-#define NG_ASSET_STRIPS_8 8  /* sprite_p3_plane.png */
+#define NG_ASSET_STRIPS_8 2  /* sprite_p3_plane.png */
 #define NG_ASSET_STRIPS_9 2  /* sprite_shot_player.png */
-#define NG_ASSET_STRIPS_10 10  /* opponent_bomber_olive.png */
-#define NG_ASSET_STRIPS_11 10  /* opponent_boss_crimson_cathedral.png */
-#define NG_ASSET_STRIPS_12 16  /* opponent_boss_gold_core.png */
-#define NG_ASSET_STRIPS_13 14  /* opponent_boss_heli_carrier.png */
-#define NG_ASSET_STRIPS_14 8  /* opponent_boss_navy_battleship.png */
-#define NG_ASSET_STRIPS_15 12  /* opponent_boss_red_fortress.png */
-#define NG_ASSET_STRIPS_16 14  /* opponent_boss_stealth_bomber.png */
-#define NG_ASSET_STRIPS_17 11  /* opponent_boss_tank_fortress.png */
-#define NG_ASSET_STRIPS_18 4  /* opponent_drone_red.png */
-#define NG_ASSET_STRIPS_19 8  /* opponent_fighter_grayred.png */
-#define NG_ASSET_STRIPS_20 8  /* opponent_heli_gunship.png */
-#define NG_ASSET_STRIPS_21 8  /* opponent_interceptor_blackgold.png */
-#define NG_ASSET_STRIPS_22 6  /* opponent_missile_boat.png */
+#define NG_ASSET_STRIPS_10 2  /* opponent_bomber_olive.png */
+#define NG_ASSET_STRIPS_11 5  /* opponent_boss_crimson_cathedral.png */
+#define NG_ASSET_STRIPS_12 7  /* opponent_boss_gold_core.png */
+#define NG_ASSET_STRIPS_13 7  /* opponent_boss_heli_carrier.png */
+#define NG_ASSET_STRIPS_14 5  /* opponent_boss_navy_battleship.png */
+#define NG_ASSET_STRIPS_15 7  /* opponent_boss_red_fortress.png */
+#define NG_ASSET_STRIPS_16 7  /* opponent_boss_stealth_bomber.png */
+#define NG_ASSET_STRIPS_17 5  /* opponent_boss_tank_fortress.png */
+#define NG_ASSET_STRIPS_18 2  /* opponent_drone_red.png */
+#define NG_ASSET_STRIPS_19 2  /* opponent_fighter_grayred.png */
+#define NG_ASSET_STRIPS_20 2  /* opponent_heli_gunship.png */
+#define NG_ASSET_STRIPS_21 2  /* opponent_interceptor_blackgold.png */
+#define NG_ASSET_STRIPS_22 2  /* opponent_missile_boat.png */
 #define NG_ASSET_STRIPS_23 2  /* opponent_shot_orb.png */
-#define NG_ASSET_STRIPS_24 4  /* opponent_shot_ring.png */
-#define NG_ASSET_STRIPS_25 6  /* opponent_tank_camo.png */
+#define NG_ASSET_STRIPS_24 2  /* opponent_shot_ring.png */
+#define NG_ASSET_STRIPS_25 2  /* opponent_tank_camo.png */
 
 static const NGSpriteAssetMeta g_ng_asset_meta[NG_ASSET_META_COUNT] = {
     { 0, 16, 16, 16, 0, 0, 0, 0, 256, 256, 0, 0 }, /* background_coast_city.png */
     { 256, 17, 16, 16, 0, 0, 0, 0, 256, 256, 0, 0 }, /* background_sky_mountains.png */
     { 512, 18, 10, 15, 3, 1, 0, 0, 160, 240, 1, 0 }, /* sprite_p1_pilot.png */
-    { 768, 19, 8, 10, 4, 6, 8, 0, 112, 160, 1, 0 }, /* sprite_p1_plane.png */
+    { 768, 19, 2, 2, 0, 0, 5, 0, 22, 32, 1, 0 }, /* sprite_p1_plane.png */
     { 1024, 20, 10, 15, 3, 1, 0, 0, 160, 240, 1, 0 }, /* sprite_p2_pilot.png */
-    { 1280, 21, 8, 9, 4, 7, 8, 0, 112, 144, 1, 0 }, /* sprite_p2_plane.png */
+    { 1280, 21, 2, 2, 0, 0, 3, 0, 25, 32, 1, 0 }, /* sprite_p2_plane.png */
     { 1536, 22, 10, 15, 3, 1, 0, 0, 160, 240, 1, 0 }, /* sprite_p3_pilot.png */
-    { 1792, 23, 8, 9, 4, 7, 8, 0, 112, 144, 1, 0 }, /* sprite_p3_plane.png */
+    { 1792, 23, 2, 2, 0, 0, 3, 0, 25, 32, 1, 0 }, /* sprite_p3_plane.png */
     { 2048, 24, 2, 3, 7, 13, 12, 8, 8, 40, 1, 0 }, /* sprite_shot_player.png */
-    { 2304, 25, 10, 10, 3, 6, 8, 0, 144, 160, 1, 3 }, /* opponent_bomber_olive.png */
-    { 2560, 26, 10, 16, 3, 0, 7, 0, 146, 256, 1, 3 }, /* opponent_boss_crimson_cathedral.png */
-    { 2816, 27, 16, 15, 0, 1, 8, 0, 240, 240, 1, 3 }, /* opponent_boss_gold_core.png */
-    { 3072, 28, 14, 16, 1, 0, 13, 0, 197, 256, 1, 3 }, /* opponent_boss_heli_carrier.png */
-    { 3328, 29, 8, 16, 4, 0, 2, 0, 124, 256, 1, 3 }, /* opponent_boss_navy_battleship.png */
-    { 3584, 30, 12, 16, 2, 0, 1, 0, 190, 256, 1, 3 }, /* opponent_boss_red_fortress.png */
-    { 3840, 31, 14, 16, 1, 0, 9, 0, 205, 256, 1, 3 }, /* opponent_boss_stealth_bomber.png */
-    { 4096, 32, 11, 16, 2, 0, 15, 0, 161, 256, 1, 3 }, /* opponent_boss_tank_fortress.png */
-    { 4352, 33, 4, 4, 6, 12, 4, 8, 56, 56, 1, 3 }, /* opponent_drone_red.png */
-    { 4608, 34, 8, 9, 4, 7, 0, 0, 128, 144, 1, 3 }, /* opponent_fighter_grayred.png */
-    { 4864, 35, 8, 9, 4, 7, 8, 0, 112, 144, 1, 3 }, /* opponent_heli_gunship.png */
-    { 5120, 36, 8, 9, 4, 7, 12, 0, 104, 144, 1, 3 }, /* opponent_interceptor_blackgold.png */
-    { 5376, 37, 6, 10, 5, 6, 12, 0, 72, 160, 1, 3 }, /* opponent_missile_boat.png */
-    { 5632, 38, 2, 2, 7, 14, 0, 0, 32, 32, 1, 3 }, /* opponent_shot_orb.png */
-    { 5888, 39, 4, 3, 6, 13, 12, 8, 40, 40, 1, 3 }, /* opponent_shot_ring.png */
-    { 6144, 40, 6, 8, 5, 8, 8, 8, 80, 120, 1, 3 }, /* opponent_tank_camo.png */
+    { 2304, 25, 2, 2, 0, 0, 1, 0, 29, 32, 1, 3 }, /* opponent_bomber_olive.png */
+    { 2560, 26, 5, 7, 1, 0, 8, 0, 64, 112, 1, 3 }, /* opponent_boss_crimson_cathedral.png */
+    { 2816, 27, 7, 7, 0, 0, 0, 0, 112, 112, 1, 3 }, /* opponent_boss_gold_core.png */
+    { 3072, 28, 7, 7, 0, 0, 13, 0, 86, 112, 1, 3 }, /* opponent_boss_heli_carrier.png */
+    { 3328, 29, 5, 7, 1, 0, 13, 0, 54, 112, 1, 3 }, /* opponent_boss_navy_battleship.png */
+    { 3584, 30, 7, 7, 0, 0, 14, 0, 83, 112, 1, 3 }, /* opponent_boss_red_fortress.png */
+    { 3840, 31, 7, 7, 0, 0, 11, 0, 90, 112, 1, 3 }, /* opponent_boss_stealth_bomber.png */
+    { 4096, 32, 5, 7, 1, 0, 5, 0, 70, 112, 1, 3 }, /* opponent_boss_tank_fortress.png */
+    { 4352, 33, 2, 2, 0, 0, 0, 0, 32, 32, 1, 3 }, /* opponent_drone_red.png */
+    { 4608, 34, 2, 2, 0, 0, 2, 0, 28, 32, 1, 3 }, /* opponent_fighter_grayred.png */
+    { 4864, 35, 2, 2, 0, 0, 3, 0, 25, 32, 1, 3 }, /* opponent_heli_gunship.png */
+    { 5120, 36, 2, 2, 0, 0, 4, 0, 23, 32, 1, 3 }, /* opponent_interceptor_blackgold.png */
+    { 5376, 37, 2, 2, 0, 0, 9, 0, 14, 32, 1, 3 }, /* opponent_missile_boat.png */
+    { 5632, 38, 2, 2, 0, 0, 0, 0, 32, 32, 1, 3 }, /* opponent_shot_orb.png */
+    { 5888, 39, 2, 2, 0, 0, 0, 0, 32, 32, 1, 3 }, /* opponent_shot_ring.png */
+    { 6144, 40, 2, 2, 0, 0, 5, 0, 21, 32, 1, 3 }, /* opponent_tank_camo.png */
 };
 
 #endif
